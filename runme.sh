69424a818e99fe6ebdba425790e768af9ed56cc9
tuapuikia <dknyvice@gmail.com>
2022-09-21 15:12:58 +0800

    Use the latest openconnect-sso 0.8.0 version and spoof the anyconnect linux agent by default

--- runme.sh
+++ runme.sh
@@ -1,7 +1,19 @@
 #!/bin/bash -x
 
+CUSTOM_USER_AGENT="$(echo $customagent)"
+
+DEFAULT_USER_AGENT="AnyConnect Linux_64 4.10.01075"
+
 INPUT=$1
 
-/usr/local/bin/openconnect-sso $INPUT
+if [ ! -z "$CUSTOM_USER_AGENT" ]; then
+  USER_AGENT=$CUSTOM_USER_AGENT
+else
+  USER_AGENT=$DEFAULT_USER_AGENT
+fi
+
+echo "Using $USER_AGENT as user-agent"
+
+/usr/local/bin/openconnect-sso $INPUT -- --useragent $USER_AGENT
 
 sleep 30
